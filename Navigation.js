524cb28ca544e2a519a9ca43f8322a7bba2bbb19
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2024-01-12 10:51:27 -0500

    Setting up navigation

---
+++ Navigation.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
+
+export default function Navigation() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
